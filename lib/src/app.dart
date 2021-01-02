acb7fb46b7b38c333da3f29eb97db00ec99ff1c5
aapetsi <apetsiampiah@gmail.com>
2021-01-02 19:03:24 +0000

    complete tutorial project

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 // import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -11,17 +15,25 @@ class App extends StatefulWidget {
 // this class must extend the StatelessWidget base class
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = new ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text(
